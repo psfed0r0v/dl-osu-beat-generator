be3778ca7a8133c1e67961715c9db41d7efa9c4b
Pavel Fedorov <pavel.fedorov@advlab.io>
2020-12-16 19:11:43 +0300

    fix mode and pathes and range

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ MODE = params['MODE']
 def main():
     cur = os.getcwd()
     Path(cur + '/cutted_data').mkdir(parents=True, exist_ok=True)
-    Path(cur + '/cutted_data/audio').mkdir(parents=True, exist_ok=True)
-    Path(cur + '/cutted_data/text').mkdir(parents=True, exist_ok=True)
+    Path(cur + f'/cutted_data/audio_{MODE}').mkdir(parents=True, exist_ok=True)
+    Path(cur + f'/cutted_data/text_{MODE}').mkdir(parents=True, exist_ok=True)
 
     audios = [f for f in os.listdir(DATA_PATH + 'audio_' + MODE) if isfile(join(DATA_PATH + 'audio_' + MODE, f))]
-    for i in range(len(audios)):
+    for i in range(1, len(audios)):
         sound = AudioSegment.from_mp3(DATA_PATH + f'audio_{MODE}/' + str(i) + '.mp3')
         sound.export(DATA_PATH + f'audio_{str(i)}.wav', format='wav')
         tmp = SplitWavAudio(f'audio_{str(i)}.wav', f'text_{MODE}/' + str(i) + '.osu', DATA_PATH, 'cutted_data/')
