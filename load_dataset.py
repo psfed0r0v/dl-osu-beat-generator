0229db22be2a12d21ef65aebd7040aa1d89daeba
artakhm <46814543+artakhm@users.noreply.github.com>
2020-12-16 18:34:43 +0300

    Add files via upload

---
+++ load_dataset.py
@@ -0,0 +1,36 @@
+def Norm(mel, mean, std, eps=1e-8):
+  mel = torch.log(mel + eps)
+  mel = (mel - mean) / std
+  return mel
+
+class dataset(Dataset):
+    def __init__(self, wav_dir, n_mels, transform=None):
+        data = [d for d in os.listdir(wav_dir)]
+        self.data = data
+        self.transform = transform
+        self.n_mels = n_mels
+
+    def __len__(self):
+        return len(self.data)
+    
+    def noise(self,wav):
+      wav += 0.01 * torch.randn(wav.shape)
+      return wav
+
+    def __getitem__(self, index):
+        path = self.data[index]
+        wav, sr = torchaudio.load(path + '/audio.wav')
+        wav = torch.mean(wav,dim=0)
+        points = pd.read_csv(path + '/audio.txt', sep=" ")
+        target = np.zeros(5000)
+        target[point] = 1.0 for point in points
+
+        if self.transform:
+            wav = self.transform(wav)
+            wav = self.noise(wav)
+
+        featurizer = torchaudio.transforms.MelSpectrogram(n_mels=self.n_mels)
+        mels = featurizer(wav)
+        mels = Norm(mels,torch.mean(mels),torch.std(mels))
+        
+        return mels, torch.tensor(target)
---
+++ train.py
@@ -0,0 +1,45 @@
+import wandb
+import torch 
+# import torchvision
+from torch import nn
+# !pip install torchaudio
+# !pip install wandb
+import torchaudio
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+
+
+
+def train(model, N_EPOCHS, trainloader, lr=0.001, BATCH_SIZE=128, ITER_LOG=None):
+    if not ITER_LOG:
+      ITER_LOG=trainloader.__len__()-1
+      
+    now = datetime.datetime.now()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr, betas=(0.5, 0.999))
+    criterion = nn.CrossEntropyLoss()
+    
+    for epoch in range(N_EPOCHS):
+        loss_log = 0.0
+        for i, data in enumerate(trainloader, 0):
+            mels, labels = data[0].to(device), data[1].to(device)
+            pred = model(mels)
+
+            optimizer.zero_grad()
+            loss = criterion(pred,labels)
+            loss_log += loss.item()
+            loss.backward()
+            optimizer.step()
+                
+            if  i%ITER_LOG == ITER_LOG-1:    
+                wandb.log({"loss": loss_log / ITER_LOG})
+                
+                print('[%d, %5d] Running loss: %.3f' %
+                    (epoch + 1, i + 1, loss_log / ITER_LOG))
+                loss_log = 0.0
+
+                print('time:',datetime.datetime.now() - now)
+                now = datetime.datetime.now()
+            
+                print(' ')
+
+    print('Finished Training')
