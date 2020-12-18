0ad5f6bba4833b6a6bf91ab0f1f63b48b4146027
artakhm <46814543+artakhm@users.noreply.github.com>
2020-12-18 17:18:14 +0300

    Add files via upload

--- form_dataset.py
+++ form_dataset.py
@@ -2,8 +2,10 @@
 from utils.read_data import parse_data
 from utils import make_dataset
 from config import get_params
+import os
 
 params = get_params()
+os.mkdir(params.DATA_PATH)
 
 if __name__ == '__main__':
     make_dataset(params.OSU_TRACKS_DIR, params.DATA_PATH + 'audio_normal', + params.DATA_PATH + 'text_normal', params.ENUMERATE_FROM)
