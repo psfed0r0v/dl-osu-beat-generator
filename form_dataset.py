80512233e0a823320af12653ca2026b705b55e37
artakhm <46814543+artakhm@users.noreply.github.com>
2020-12-20 02:04:47 +0300

    Update form_dataset.py

--- form_dataset.py
+++ form_dataset.py
@@ -1,6 +1,6 @@
 
 from utils.read_data import parse_data
-from utils.make_dataset import make_dataset
+from utils.make_dataset import make_dataset, lower_quality
 from config import get_params
 import os
 
@@ -10,3 +10,5 @@ os.mkdir(params.DATA_PATH)
 if __name__ == '__main__':
     make_dataset(params.OSU_TRACKS_DIR, params.DATA_PATH + 'audio_normal', params.DATA_PATH + 'text_normal', params.ENUMERATE_FROM)
     parse_data()
+    cur = os.getcwd()
+    lower_quality(cur + f'/cutted_data/audio_{params.MODE}, params.SAMPLE_RATE)
