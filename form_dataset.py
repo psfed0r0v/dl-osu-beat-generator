a699316c442a31f5d6de0dc9e03c03a9f80e3b72
artakhm <46814543+artakhm@users.noreply.github.com>
2020-12-19 23:01:56 +0300

    Update form_dataset.py

--- form_dataset.py
+++ form_dataset.py
@@ -1,6 +1,6 @@
 
 from utils.read_data import parse_data
-from utils import make_dataset
+from utils.make_dataset import make_dataset
 from config import get_params
 import os
 
@@ -8,6 +8,5 @@ params = get_params()
 os.mkdir(params.DATA_PATH)
 
 if __name__ == '__main__':
-    print(params.DATA_PATH + 'audio_normal')
     make_dataset(params.OSU_TRACKS_DIR, params.DATA_PATH + 'audio_normal', params.DATA_PATH + 'text_normal', params.ENUMERATE_FROM)
     parse_data()
